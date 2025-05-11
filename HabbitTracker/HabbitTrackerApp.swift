155c6528432c2c70497b30979ced33a3a459a4ff
BumBaZo86 <lina.bergsten86@live.se>
2025-05-12 00:18:39 +0200

    Initial Commit

---
+++ HabbitTracker/HabbitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitTrackerApp.swift
+//  HabbitTracker
+//
+//  Created by Lina Bergsten on 2025-05-12.
+//
+
+import SwiftUI
+
+@main
+struct HabbitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabbitTrackerTests/HabbitTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  HabbitTrackerTests.swift
+//  HabbitTrackerTests
+//
+//  Created by Lina Bergsten on 2025-05-12.
+//
+
+import Testing
+
+struct HabbitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
